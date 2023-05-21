95f9bcbf9a33fb12222b446cf5dd68ab4d1127ea
Pedro Vitor <93941657+PedroVitorCarFerSilva@users.noreply.github.com>
2023-05-21 16:05:14 -0300

    Update Zombie and Morty code.py

--- Zombie and Morty code.py
+++ Zombie and Morty code.py
@@ -1,10 +1,11 @@
 #code by Iraikare
 #graphics by João Ricardo
+#modified by Pedro Vitor (Roger)
 
 import pygame
 from pygame.locals import *
 import os
-from random import randrange, choice
+from random import randrange, choice, randint
 pygame.font.init()
 pygame.mixer.init()
 pygame.init()
@@ -23,15 +24,20 @@ vermelho = (255, 0, 0)
 marrom = (209, 169, 161)
 velocidade = 40
 pontos = 0
-velocidade_jogo = 7
+velocidade_jogo = 8
 
 fundo = pygame.transform.scale(pygame.image.load(os.path.join(diretorio_imagens,'cenario1f.png')), (LARGURA, ALTURA))
 spritesheet = pygame.image.load(os.path.join(diretorio_imagens,'tudinho.png')).convert_alpha()
+spritepredios = pygame.transform.scale(pygame.image.load(os.path.join(diretorio_imagens,'predios.png')), (LARGURA, ALTURA))
 
 som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons,'gameover1.wav'))
 som_colisao.set_volume(0.2)
 som_pontuacao = pygame.mixer.Sound(os.path.join(diretorio_sons,'score.wav'))
 som_pontuacao.set_volume(0.2)
+
+#music = pygame.mixer.music.load('sons/musca.wav')
+#pygame.mixer.music.set_volume(0.13)
+#pygame.mixer.music.play(-1)
 
 start = False
 colidiu = False
@@ -51,7 +57,10 @@ legenda = FONTE2.render(f"Aperte a tecla 'espaço' para pular!", 1, branco)
 def reiniciar_jogo():
     global pontos, velocidade_jogo, colidiu, escolha_obstaculo, fimdejogo
     pontos = 0
-    velocidade_jogo = 7
+    velocidade_jogo = 8
+    personagem.velocidade_pulo = 15
+    personagem.altura_pulo = 180
+    personagem.rect.center = (490,430)
     colidiu = False
     fimdejogo = False
     personagem.rect.y = 370
@@ -72,6 +81,9 @@ class Personagem(pygame.sprite.Sprite):
             img = pygame.transform.scale(img,(64*2, 64*2))
             self.imagens_personagem.append(img)
 
+        self.altura_pulo = 180
+        self.velocidade_pulo = 15
+
         self.index_lista = 0
         self.image = self.imagens_personagem[self.index_lista]
         self.rect = self.image.get_rect()
@@ -79,26 +91,49 @@ class Personagem(pygame.sprite.Sprite):
         self.pos_y_inicial = 370
         self.rect.center = (490,430)
         self.pulo = False
+        self.framespeed = 0.2
 
     def pular(self):
         self.pulo = True
         self.som_pulo.play()
+
+    def movemento(self, vel):
+        self.framespeed = 0.2
+        if self.rect.topright[0] <= LARGURA:
+            if pygame.key.get_pressed()[pygame.K_d] or pygame.key.get_pressed()[pygame.K_RIGHT]:
+                if self.pulo:
+                    self.rect.x += 4
+                else:
+                    self.rect.x += 8
+        if self.rect.x >= -10:
+            if pygame.key.get_pressed()[pygame.K_a] or pygame.key.get_pressed()[pygame.K_LEFT]:
+                if self.pulo:
+                    self.rect.x -= 4
+                else:
+                    self.rect.x -= vel
+                    self.framespeed = 0
+                    self.index_lista = 0
+        if self.pulo:
+            self.framespeed = 0
+            self.index_lista = 3
  
-    def update(self):
+    def update(self, vel):
+        self.movemento(vel)
         if start == True:
+            
             if self.pulo == True:
-                if self.rect.y <= 190:
+                if self.rect.y <= self.altura_pulo:
                     self.pulo = False
-                self.rect.y -= 15
+                self.rect.y -= self.velocidade_pulo
             else:
                 if self.rect.y < self.pos_y_inicial:
-                    self.rect.y += 15
+                    self.rect.y += self.velocidade_pulo
                 else:
                     self.rect.y = self.pos_y_inicial
 
-            if self.index_lista > 2:
+            self.index_lista += self.framespeed
+            if self.index_lista > 4:
                 self.index_lista = 0
-            self.index_lista += 0.25
             self.image = self.imagens_personagem[int(self.index_lista)]
 
 class Zombie(pygame.sprite.Sprite):
@@ -118,9 +153,9 @@ class Zombie(pygame.sprite.Sprite):
  
     def update(self):
         if start == True:
-            if self.index_lista > 2:
+            self.index_lista += 0.2
+            if self.index_lista > 4:
                 self.index_lista = 0
-            self.index_lista += 0.25
             self.image = self.imagens_zombie[int(self.index_lista)]
 
 class Chao(pygame.sprite.Sprite):
@@ -134,7 +169,7 @@ class Chao(pygame.sprite.Sprite):
         if start == True:
             if self.rect.topright[0] < 0:
                 self.rect.x = LARGURA
-            self.rect.x -= 10
+            self.rect.x -= velocidade_jogo
 
 class Nuvens(pygame.sprite.Sprite):
     def __init__(self, pos_x):
@@ -148,7 +183,20 @@ class Nuvens(pygame.sprite.Sprite):
         if start == True:
             if self.rect.topright[0] < 0:
                 self.rect.x = LARGURA
-            self.rect.x -= velocidade_jogo
+            self.rect.x -= velocidade_jogo // 8
+
+class Predios(pygame.sprite.Sprite):
+    def __init__(self, pos_x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = spritepredios
+        self.rect = self.image.get_rect()
+        self.rect.x = pos_x
+        self.rect.y = 0
+    def update(self):
+        if start == True:
+            if self.rect.topright[0] < 0:
+                self.rect.x = LARGURA
+            self.rect.x -= velocidade_jogo // 4
         
 class Lixo(pygame.sprite.Sprite):
     def __init__(self):
@@ -207,24 +255,29 @@ for i in range(5):
     todas_as_sprites.add(nuvens)
 for i in range(20):
     chao = Chao(i)
-    todas_as_sprites.add(chao)  
+    todas_as_sprites.add(chao)
+for i in range(2):
+    predios = Predios((i-1)*LARGURA)
+    todas_as_sprites.add(predios)
 lixo = Lixo()
 cerca = Cerca()
 banco = Banco()
-todas_as_sprites.add(lixo, cerca, banco)
+todas_as_sprites.add(lixo, cerca, banco, predios)
 for i in range(3):
     zombie = Zombie(i)
     todas_as_sprites.add(zombie)
 personagem = Personagem()
-todas_as_sprites.add(personagem)
+personagem_s = pygame.sprite.Group()
+personagem_s.add(personagem)
 
 grupo_obstaculos = pygame.sprite.Group()
-grupo_obstaculos.add(lixo, cerca, banco)
+grupo_obstaculos.add(lixo, cerca, banco, zombie)
 
 def janela_desenho():
     JANELA.blit(fundo, (0,0))
     grupo_obstaculos.draw(JANELA)
     todas_as_sprites.draw(JANELA)
+    personagem_s.draw(JANELA)
     
     global pontos
     pontostxt =  FONTE.render("pontos: " + str(pontos), 1, branco)
@@ -244,6 +297,7 @@ def main():
     global start
     relogio = pygame.time.Clock()
     rodando = True
+    pause = False
     while rodando:
         relogio.tick(velocidade)
         for event in pygame.event.get():
@@ -251,13 +305,19 @@ def main():
                 rodando = False
             if event.type == KEYDOWN:
                 start = True
-                if event.key == K_SPACE and colidiu == False:
-                    if personagem.rect.y != personagem.pos_y_inicial:
-                        pass
-                    else:
-                        personagem.pular()
+                if event.key == K_SPACE or event.key == K_UP or event.key == K_w:
+                    if colidiu == False and pause == False:
+                        if personagem.rect.y != personagem.pos_y_inicial:
+                            pass
+                        else:
+                            personagem.pular()
                 if event.key == K_r and colidiu == True:
                     reiniciar_jogo()
+                if event.key == K_p and colidiu == False:
+                    if pause:
+                        pause = False
+                    else:
+                        pause = True
 
         colisoes = pygame.sprite.spritecollide(personagem, grupo_obstaculos, False, pygame.sprite.collide_mask)
 
@@ -280,18 +340,20 @@ def main():
             som_colisao.play()
             colidiu = True
 
-        if colidiu == True:
+        if colidiu:
             fimdejogo = True
-        elif start == True:
+        elif start and pause == False:
             pontos +=1
-            todas_as_sprites.update()   
+            todas_as_sprites.update()
+            personagem_s.update(velocidade_jogo)
 
-        if pontos %100 == 0 and colidiu == False and start == True:
-            som_pontuacao.play()
-            if velocidade_jogo >= 30:
-                velocidade_jogo += 0
-            else:
-                velocidade_jogo += 1
+            if pontos %100 == 0:
+                som_pontuacao.play()
+                if velocidade_jogo >= 40:
+                    velocidade_jogo += 0
+                else:
+                    velocidade_jogo += 1
+                    personagem.altura_pulo += 2
 
     pygame.quit()
 
